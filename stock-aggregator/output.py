f565d0fc9c14d016a69958d6c0501fd02656f668
hysdsk <26004436+hysdsk@users.noreply.github.com>
2023-09-11 21:08:59 +0900

    chore: 直近一分間の売買代金と板更新数を項目に追加する

--- stock-aggregator/output.py
+++ stock-aggregator/output.py
@@ -1,5 +1,9 @@
+from datetime import datetime, time, timedelta
+
+
 class Output(object):
     def __init__(self) -> None:
+        self.lastminutehistories: list[TradingValueHistory] = []
         self.buy_price = None
         self.buy_time = None
         self.buy_vwap = None
@@ -7,6 +11,8 @@ class Output(object):
         self.buy_count_after = 0
         self.buy_status = None
         self.buy_tradingvalue = None
+        self.buy_tradingvalue_byminute = 0
+        self.buy_updatecount_byminute = 0
         self.sell_price = None
         self.sell_time = None
         self.sell_vwap = None
@@ -16,6 +22,8 @@ class Output(object):
         self.sell_count_before = 0
         self.sell_status = None
         self.sell_tradingvalue = None
+        self.sell_tradingvalue_byminute = 0
+        self.sell_updatecount_byminute = 0
         self.high_price = None
         self.low_price = None
         self.high_price_time = None
@@ -25,3 +33,28 @@ class Output(object):
         self.preparing_m_order_count = 0
         self.resting_m_order_time = None
         self.resting_m_order_count = 0
+
+    def set_buy_lastminute(self):
+        self.buy_tradingvalue_byminute = self.tradingvalue_byminute()
+        self.buy_updatecount_byminute = self.updatecount_byminute()
+
+    def set_sell_lastminute(self):
+        self.sell_tradingvalue_byminute = self.tradingvalue_byminute()
+        self.sell_updatecount_byminute = self.updatecount_byminute()
+
+    def add_lastminutehistories(self, receivedtime: datetime, tradingvalue: int):
+        self.lastminutehistories.append(TradingValueHistory(receivedtime.time(), tradingvalue))
+        recenttime = receivedtime - timedelta(minutes=1)
+        while self.lastminutehistories[0].receivedtime < recenttime.time():
+            del self.lastminutehistories[0]
+
+    def tradingvalue_byminute(self):
+        return sum([h.tradingvalue for h in self.lastminutehistories])
+    
+    def updatecount_byminute(self):
+        return len(self.lastminutehistories)
+
+class TradingValueHistory(object):
+    def __init__(self, receivedtime: time, tradingvalue: int):
+        self.receivedtime: time = receivedtime
+        self.tradingvalue: int = tradingvalue
